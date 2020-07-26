6b4633c0318acccda763ae6d6b2bf56e5cd9f014
Víctor Colombo <victorcora98@gmail.com>
2020-07-26 14:48:53 -0300

    Implement winning condition

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(function () {
-    generateNewGame();
+    let game = generateNewGame();
 
     $(".cell").click(function () {
         unselectPreviousCell();
@@ -8,9 +8,12 @@ $(function () {
 
     $(document).keypress(function (event) {
         let selectedCell = $(".selected");
-        if (event.key >= 1 && event.key <= 9 && !selectedCell.hasClass("value-locked")) {
-            $(".value", selectedCell).text(event.key);
-            checkGameFinished();
+        let valueElement = $(".value", selectedCell)
+        if (event.key >= 1 && event.key <= 9 && !valueElement.hasClass("value-locked")) {
+            valueElement.text(event.key);
+            if (checkGameFinished(game)) {
+                console.log("You Win");
+            }
         }
     });
 });
@@ -27,6 +30,8 @@ function generateNewGame() {
             }
         }
     }
+
+    return game;
 }
 
 function unselectPreviousCell() {
@@ -37,6 +42,42 @@ function selectCell(cell) {
     $(cell).addClass("selected");
 }
 
-function checkGameFinished() {
-    // Todo
+function checkGameFinished(game) {
+    if (isAnyCellEmpty() || isUserGameEqualToSolution(game)) {
+        return false;
+    }
+    
+    return true;
+}
+
+function isUserGameEqualToSolution(game) {
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            let cell = $(".row").eq(row).children().eq(col);
+            let value = $(".value", cell).text();
+            if (value != game.solution[row][col]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+function isAnyCellEmpty() {
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            let cell = $(".row").eq(row).children().eq(col);
+            let valueElement = $(".value", cell)
+            if (isEmpty(valueElement)) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+function isEmpty(element) {
+    return !$.trim(element.html())
 }
