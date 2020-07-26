464fdc8e267a73dc3d709ae6e671fd90269ec9bf
Víctor Colombo <victorcora98@gmail.com>
2020-07-26 13:57:46 -0300

    Fix issue where input was not being set to correct html element

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ $(function () {
     });
 
     $(document).keypress(function (event) {
-        if (event.key >= 1 && event.key <= 9 && !$(".selected").hasClass("value-locked")) {
-            $(".selected").text(event.key);
+        let selectedCell = $(".selected");
+        if (event.key >= 1 && event.key <= 9 && !selectedCell.hasClass("value-locked")) {
+            $(".value", selectedCell).text(event.key);
             checkGameFinished();
         }
     });
@@ -20,9 +21,9 @@ function generateNewGame() {
     for (let row = 0; row < 9; row++) {
         for (let col = 0; col < 9; col++) {
             if (game.table[row][col] !== 0) {
-                let cell = $('.row').eq(row).children().eq(col);
-                $(cell).text(game.table[row][col]);
-                $(cell).addClass("value-locked");
+                let cell = $(".row").eq(row).children().eq(col);
+                $(".value", cell).text(game.table[row][col]);
+                $(".value", cell).addClass("value-locked");
             }
         }
     }
